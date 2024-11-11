f2e8ff5cc64404bdd04efa2083bda666f1c0b272
PedroMG2023 <devmgcoding03@gmail.com>
2024-11-10 23:32:18 -0600

    Agregado prototipo vista empleado producto

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():  # put application's code here
-    return 'Hello World!'
+    return render_template("index.html")
+
+@app.route('/EmpleadoProducto')
+def EmpleadoProducto():  # put application's code here
+    return render_template("VistasEmpleado/IngresarProducto.html")
+
+@app.route('/EmpleadoFactura')
+def EmpleadoFactura():  # put application's code here
+    return render_template("VistasEmpleado/EmpleadoFactura.html")
 
 
 if __name__ == '__main__':
